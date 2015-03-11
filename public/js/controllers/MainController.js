d43cefad56aea93c6e801ff852d884b827e5d5ee
Newms34 <Newms3450@msn.com>
2015-03-11 17:32:40 -0400

    initial commit (sorta)

--- public/js/controllers/MainController.js
+++ public/js/controllers/MainController.js
@@ -1,31 +1,51 @@
 var app = angular.module("Ship", []);
 
 
-app.controller("MainController", function($scope, socket) {
-
+app.controller("MainController", function($scope) {
+    $scope.tunnelEls = [{
+        size: '5vh'
+    }, {
+        size: '10vh'
+    }, {
+        size: '15vh'
+    }, {
+        size: '20vh'
+    }, {
+        size: '25vh'
+    }, {
+        size: '30vh'
+    }, {
+        size: '35vh'
+    }, {
+        size: '40vh'
+    }, {
+        size: '45vh'
+    }, {
+        size: '50vh',
+        color: '#000900'
+    }, {
+        size: '55vh',
+        color: '#000900'
+    }, {
+        size: '60vh',
+        color: '#000900'
+    }, {
+        size: '65vh',
+        color: '#000900'
+    }, {
+        size: '70vh',
+        color: '#000900'
+    }, {
+        size: '75vh',
+        color: '#000900'
+    }, {
+        size: '80vh',
+        color: '#000900'
+    }, {
+        size: '85vh',
+        color: '#000900'
+    }, {
+        size: '90vh',
+        color: '#000900'
+    }];
 });
-
-//I'm pretty sure the following is all included in SocketFactory. But just in case, for now...
-// app.factory('socketFactory', function($rootScope) {
-//     var socket = io.connect();
-//     return {
-//         on: function(eventName, callback) {
-//             socket.on(eventName, function() {
-//                 var args = arguments;
-//                 $rootScope.$apply(function() {
-//                     callback.apply(socket, args);
-//                 });
-//             });
-//         },
-//         emit: function(eventName, data, callback) {
-//             socket.emit(eventName, data, function() {
-//                 var args = arguments;
-//                 $rootScope.$apply(function() {
-//                     if (callback) {
-//                         callback.apply(socket, args);
-//                     }
-//                 });
-//             })
-//         }
-//     };
-// });
--- public/js/socket.js
+++ public/js/socket.js
@@ -1,11 +1,73 @@
 var socket = io();
+var isPhone = detectmob();
+var allToShade = document.getElementsByClassName('shadeMe');
+var rotsX = [9.46, 9.46, 9.46, 9.46, 0, 0, 0, 0, 33.7, 0, 90];
+var rotsY = [0, 90, 270, 180, 90, 90, 270, 270, 180, 180, 0];
 
-window.addEventListener('deviceorientation', function(e) {
-    var pitch = Math.floor((Math.max(0, Math.min(e.beta, 90)) / 90) * 360)
-    socket.emit('controlMove', pitch);
-    $('#freqMon').val(pitch);
-})
-socket.on('moveShip', function(allFreqs) {
-    //do stuff with the ship. Move, etc.
+//on page load, check if user is using a phone. If they are, this render will be the controller. Otherwise, it's the viewer
+function detectmob() {
+    if (navigator.userAgent.match(/Android/i) || navigator.userAgent.match(/webOS/i) || navigator.userAgent.match(/iPhone/i) || navigator.userAgent.match(/iPad/i) || navigator.userAgent.match(/iPod/i) || navigator.userAgent.match(/BlackBerry/i) || navigator.userAgent.match(/Windows Phone/i)) {
+        return true;
+    } else {
+        return false;
+    }
+}
 
-});
+// enable vibration support
+navigator.vibrate = navigator.vibrate || navigator.webkitVibrate || navigator.mozVibrate || navigator.msVibrate;
+var buzz;
+if (navigator.vibrate) {
+    //buzz!
+}
+if (isPhone) {
+    //if it's a phone, listen for controls
+    window.addEventListener('deviceorientation', function(e) {
+        var pitch = Math.floor((Math.max(0, Math.min(e.beta, 90)) / 90) * 360);
+        socket.emit('controlMove', pitch);
+        $('#freqMon').val(pitch);
+    });
+    socket.on('crash', function() {
+        //do some fancy shmancy crashy stuff
+    });
+} else {
+    //otherwise, move the ship
+    socket.on('moveShip', function(allFreqs) {
+        //do stuff with the ship. Move, etc.
+
+    });
+}
+
+
+document.onmousemove = function(e) {
+    //move the ship
+    var x = e.y / 2;
+    var y = e.x / 2 - 90;
+    $('#shipCont').css('transform', 'rotateX(' + x + 'deg) rotateZ(' + y + 'deg)');
+    shader(x, y);
+};
+
+function shader(xIn, yIn) {
+    var allInfo = '';
+    for (var i = 0; i < allToShade.length; i++) {
+        var adjRotX = (rotsX[i] + xIn) % 360;
+        var adjRotY = (rotsY[i] + yIn) % 360;
+        var colVal = parseInt(204 - (((adjRotX + adjRotY) / 720) * 125));
+        var col = 'rgb(' + colVal + ',' + colVal + ',' + colVal + ')';
+        console.log(col);
+        // 104 dif (100 to 204)
+
+        allInfo += allToShade[i].id + ' | ' + allToShade[i].className;
+        if (window.getComputedStyle(allToShade[i]).backgroundColor != 'rgba(0, 0, 0, 0)') {
+            // allInfo += ' uses BGcolor, and is rotated (' + adjRotX + ', ' + adjRotY + ') <br/>';
+            allToShade[i].style.backgroundColor = col;
+        } else if (window.getComputedStyle(allToShade[i]).borderLeftColor !== 'rgba(0, 0, 0, 0)') {
+            // allInfo += ' uses left border color, and is rotated (' + adjRotX + ', ' + adjRotY + ') <br/>';
+            allToShade[i].style.borderLeftColor = col;
+        } else if (window.getComputedStyle(allToShade[i]).borderBottomColor !== 'rgba(0, 0, 0, 0)') {
+            // allInfo += ' uses bottom border color, and is rotated (' + adjRotX + ', ' + adjRotY + ') <br/>';
+            allToShade[i].style.borderBottomColor = col;
+        } else {
+            // allInfo += ' does not use a color(?), and is rotated (' + adjRotX + ', ' + adjRotY + ') <br/>';
+        }
+    }
+}
